54184442c61aa51c6e61252bb6483b81ae7c65a2
Aldo <aldo.medina.z@gmail.com>
2024-10-13 12:48:01 -0300

    routing bug fixed with custom userouter hok

--- app/[locale]/components/ExploreSection.tsx
+++ app/[locale]/components/ExploreSection.tsx
@@ -1,6 +1,7 @@
 "use client";
+import { useRouter } from "@/navigation";
 import { useTranslations } from "next-intl";
-import { useRouter } from "next/navigation";
+
 import React from "react";
 
 const ExploreSection = ({
